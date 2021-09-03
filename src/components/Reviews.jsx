450ec75f3daf2f4799630c35902992ead4987452
Greg-o-tron <greg.rbibo@gmail.com>
2021-09-03 13:27:01 +0100

    date changed to relative date

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { getAllReviews, getReviewsByCategory } from "../Utils/api";
+import moment from "moment";
 
 const Reviews = ({ reviews, setReviews }) => {
+
   const resetAllReviews = () => {
     getAllReviews().then((allReviews) => {
       setReviews(allReviews);
@@ -24,25 +26,25 @@ const Reviews = ({ reviews, setReviews }) => {
         <p>Please choose a category: </p>
         <button onClick={() => resetAllReviews()}>All</button>
         <button onClick={() => displayReviewsByCategory("dexterity")}>
-          dexterity
+          Dexterity
         </button>
         <button onClick={() => displayReviewsByCategory("hidden-roles")}>
-          hidden-roles
+          Hidden-roles
         </button>
         <button onClick={() => displayReviewsByCategory("strategy")}>
-          strategy
+          Strategy
         </button>
         <button onClick={() => displayReviewsByCategory("deck-building")}>
-          deck-building
+          Deck-building
         </button>
         <button onClick={() => displayReviewsByCategory("engine-building")}>
-          engine-building
+          Engine-building
         </button>
         <button onClick={() => displayReviewsByCategory("push-your-luck")}>
-          push-your-luck
+          Push-your-luck
         </button>
         <button onClick={() => displayReviewsByCategory("roll-and-write")}>
-          roll-and-write
+          Roll-and-write
         </button>
       </div>
       <ul>
@@ -50,7 +52,7 @@ const Reviews = ({ reviews, setReviews }) => {
           return (
             <li key={review.review_id}>
               <p className="category">{review.category}</p>
-              <div>Posted: {review.created_at}</div>
+              <div>Posted: {moment(review.created_at).startOf('day').fromNow()}</div>
               <img className="preview" src={review.review_img_url}></img>
               <h2>{review.title}</h2>
               <em>by: {review.owner}</em>
@@ -59,7 +61,7 @@ const Reviews = ({ reviews, setReviews }) => {
               </p>
               <p className="reviewBody">{review.review_body}</p>
               <p className="centre_buttons">
-                Votes: <button onClick={() => {}}>{review.votes}</button>{" "}
+                Votes: <button onClick={() => {}}>{review.votes}</button>
                 Reviews:{" "}
                 <button onClick={() => {}}>{review.comment_count}</button>
               </p>
